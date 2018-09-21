8aab85dac6ab0a6980c2acdb24202a21a9aaa24d
Matthew Hormis <matthewhormis@dhcp-wifi-8021x-155-41-115-185.bu.edu>
2018-09-21 00:47:31 -0400

    Updated Code

--- car_detection.py
+++ car_detection.py
@@ -1,30 +1,23 @@
-#import libraries of python opencv
 import cv2
 import numpy as np
 
-
-cap = cv2.VideoCapture('cars.mp4')
-
+capture = cv2.VideoCapture('cars.mp4')
 car_cascade = cv2.CascadeClassifier('cars.xml')
 
 while True:
 
-    ret, frame = cap.read()
+    ret, frame = capture.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_scale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    cars = car_cascade.detectMultiScale(gray, 1.1, 3)
-
+    cars = car_cascade.detectMultiScale(gray_scale, 1.1, 3)
 
     for (x,y,w,h) in cars:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)      
-
 
     cv2.imshow('video', frame)
 
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
 
-cap.release()
-
-cv2.destroyAllWindows()
+capture.release()
